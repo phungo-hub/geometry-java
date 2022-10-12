8385878450f46c0c7428eaa6033fcc63e7d8d1b5
phungo-hub <ngokieuphu@gmail.com>
2022-10-12 22:21:42 +0700

    add new files

---
+++ src/TestRectangleClass.java
@@ -0,0 +1,6 @@
+public class TestRectangleClass {
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle("blue", false, 5, 10);
+        System.out.println(rectangle.toString());
+    }
+}
